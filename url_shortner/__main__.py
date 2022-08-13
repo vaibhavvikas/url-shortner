0282b17257996bd5666ad89874813367aefa6405
Vaibhav Vikas <vbhvvikas@gmail.com>
2022-08-13 15:19:46 +0530

    Update __main__.py

--- url_shortner/__main__.py
+++ url_shortner/__main__.py
@@ -1,9 +1,11 @@
 from url_shortner import create_app
+import os
 
 
 def main():
+    port = int(os.environ.get('PORT', 5000))
     app = create_app()
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=port)
 
 
 if __name__ == "__main__":
